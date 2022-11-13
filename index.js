3c651a819c374ffa0d6cc42b13ad138b67b5d40f
Archmage13 <118137903+Archmage13@users.noreply.github.com>
2022-11-13 19:47:23 -0300

    Format code to increase readability

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 function getDate() {
-   const date = new Date();
+  const monthNames = ["January", "Frebuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  const date = new Date();
 
   const year = date.getFullYear();
   const monthIndex = date.getMonth();
   const day = date.getDate();
   const dayIndex = date.getDay();
-
-  const monthNames = ["January", "Frebuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
   const month = monthNames[monthIndex];
   const weekday = weekdays[dayIndex];
@@ -16,6 +15,20 @@ function getDate() {
   return { year, month, monthIndex, day, dayIndex, weekday };
 };
 
-const { weekday, day, month, year } = getDate();
+function getTime() {
+  const date = new Date();
 
-console.log(`Today is ${weekday}, ${day} ${month} ${year}`);
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  const formattedHour = hour >= 0 && hour < 12 ? `${hour} AM` : `${hour - 12} PM`;
+
+  return { hour, minutes, seconds, formattedHour };
+}
+
+const { weekday, day, month, year } = getDate();
+const { minutes, seconds, formattedHour } = getTime;
+
+console.log(`Today is: ${weekday}, ${day} ${month} ${year}`);
+console.log(`Current time is: ${formattedHour}:${minutes}:${seconds}`);
