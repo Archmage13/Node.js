51180e5b35188bb615a410d47efff065e1af4372
Marlon Couto <archmage13@proton.me>
2022-11-14 19:22:38 +0000

    Correct identation
    Signed-off-by: Marlon Couto <archmage13@proton.me>

--- src/utils/createChild.js
+++ src/utils/createChild.js
@@ -1,8 +1,8 @@
 export function createChild(parentElementTag, newElementTag, newElementId) {
-    const parentElement = document.querySelector(parentElementTag);
-    const newElement = document.createElement(newElementTag);
-    parentElement.appendChild(newElement);
-    if (newElementId) {
-        parentElement.lastChild.id = newElementId;
-    }
+	const parentElement = document.querySelector(parentElementTag);
+	const newElement = document.createElement(newElementTag);
+	parentElement.appendChild(newElement);
+	if (newElementId) {
+	parentElement.lastChild.id = newElementId;
+	}
 };
