fb811195ed0044fdbc579d0e04fc41bc57661919
Marlon Couto <archmage13@proton.me>
2022-11-14 04:09:26 +0000

    Refactor index.js to split code into multiple files
    Signed-off-by: Marlon Couto <archmage13@proton.me>

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
-function getDate() {
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  const date = new Date();
-
-  const year = date.getFullYear();
-  const monthIndex = date.getMonth();
-  const day = date.getDate();
-  const dayIndex = date.getDay();
-
-  const month = monthNames[monthIndex];
-  const weekday = weekdays[dayIndex];
-
-  return { year, month, monthIndex, day, dayIndex, weekday };
-};
-
-function getTime() {
-  const date = new Date();
-
-  const hour24 = date.getHours();
-  const hour12 = hour24 < 12 ? hour24 : hour24 - 12;
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-
-  const appendix = hour24 < 12 ? "AM" : "PM";
-
-  return { hour24, hour12, minutes, seconds, appendix };
-}
+import { getDate } from "./src/getDate.js";
+import { getTime } from "./src/getTime.js";
 
 const { weekday, day, month, year } = getDate();
 const { minutes, seconds, hour12, appendix } = getTime();
